be2945d9713ba33c16e674afa9efc991f4e47b07
jamie <j.parkinson@wellcome.ac.uk>
2023-01-03 17:55:17 +0000

    Permission to subscribe to stream

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -9,8 +9,30 @@ data "aws_iam_policy_document" "read_secrets" {
   }
 }
 
+data "aws_iam_policy_document" "subscribe_to_stream" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "kinesis:DescribeStream",
+      "kinesis:DescribeStreamSummary",
+      "kinesis:GetRecords",
+      "kinesis:GetShardIterator",
+      "kinesis:ListShards",
+      "kinesis:ListStreams",
+      "kinesis:SubscribeToShard"
+    ]
+    resources = [local.kinesis_stream_arn]
+  }
+}
+
 resource "aws_iam_role_policy" "read_secrets" {
   name   = "elasticsearch-log-forwarder-read-secrets"
   policy = data.aws_iam_policy_document.read_secrets.json
+  role   = module.log_forwarder.lambda_role.id
+}
+
+resource "aws_iam_role_policy" "subscribe_to_stream" {
+  name   = "elasticsearch-log-forwarder-subscribe-to-stream"
+  policy = data.aws_iam_policy_document.subscribe_to_stream.json
   role   = module.log_forwarder.lambda_role.id
 }
